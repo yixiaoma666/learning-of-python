201642a76e6117843a3b1c38e5768673be8a16cf
N/A <49421467+yixiaoma666@users.noreply.github.com>
2019-07-27 21:35:04 +0800

    Create 多元函数极值.py

---
+++ 统计/多元函数极值.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.optimize import minimize
+
+
+# 计算  (2+x1)/(1+x2) - 3*x1+4*x3 的最小值  x1,x2,x3的范围都在0.1到0.9 之间
+
+def fun(args):
+    a, b, c, d = args
+
+    def f(x):
+        return (a + x[0]) / (b + x[1]) - c * x[0] + d * x[2]
+
+    return f
+
+
+def con(args):
+    # 约束条件 分为eq 和ineq
+    # eq表示 函数结果等于0 ； ineq 表示 表达式大于等于0
+    x1min, x1max, x2min, x2max, x3min, x3max = args
+    cons = ({'type': 'ineq', 'fun': lambda x: x[0] - x1min}, {'type': 'ineq', 'fun': lambda x: -x[0] + x1max},
+            {'type': 'ineq', 'fun': lambda x: x[1] - x2min}, {'type': 'ineq', 'fun': lambda x: -x[1] + x2max},
+            {'type': 'ineq', 'fun': lambda x: x[2] - x3min}, {'type': 'ineq', 'fun': lambda x: -x[2] + x3max})
+    return cons
+
+
+if __name__ == '__main__':
+    # 定义常量值
+    args = (2, 1, 3, 4)  # a,b,c,d
+    # 设置参数范围/约束条件
+    args1 = (0.1, 0.9, 0.1, 0.9, 0.1, 0.9)  # x1min, x1max, x2min, x2max, x3min, x3max
+    cons = con(args1)
+    # 设置初始猜测值
+    x0 = np.asarray((0.5, 0.5, 0.5))
+    ans = minimize(fun(args), x0, method="SLSQP", constraints=cons)
+    print(ans)
